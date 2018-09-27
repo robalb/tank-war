040712451e26130718e4a63532fbf89e241ea842
slendy666 <slendy666@users.noreply.github.com>
2018-09-27 22:14:44 +0200

    just commented everything for you dude :p
    anyway sorry for my english ahah im almost sure that i ve done a lot of errors ahaha, but i dont mind there are the 22:14 im pretty tired so forgive me this time <3

--- mesh-viewer.js
+++ mesh-viewer.js
@@ -4,12 +4,21 @@ window.addEventListener('resize',resize);
 var canvas = null;
 var glContext = null;
 function glCheck(){
+    //
+    //get the canvas and the glContext in the meantime im checking if the browser support the gl graphics...
+    //
     canvas = document.getElementById('glContext');
     glContext = canvas.getContext('webgl');
     glContext = canvas.getContext('experimental-webgl');
+    //
+    //if he does, continue to the setup
+    //
     if(glContext){
         setup();
     }
+    //
+    //else alert
+    //
     else{
         alert('your browser do not support webgl!!');
     }
@@ -18,6 +27,9 @@ function setup(){
     
 }
 function resize(){
+    //
+    //setting the new widht/height of the canvas, and saying that the viewport of the glContext, must be equal to the drawing buffer 'capacity'(it means that you can have more accurate lines even if the window is very small[or at leas this is how i ve understood it])
+    //
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     glContext.viewport(0.0,0.0,glContext.drawingBufferWidth,glContext.drawingBufferHeight);
